f428180a93eb506129cc3b69b6f7565fdc20982f
Hassen Taidirt <htaidirt@gmail.com>
2019-09-19 15:46:33 +0200

    Change text for testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end('Hello Drone! Do you work?');
+  res.end('Hello Drone! Are you ok?');
 }).listen(3000, () => {
   console.log('Listening to port 3000');
 });

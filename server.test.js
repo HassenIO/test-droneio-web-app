adc7c228a3f7f89345a0ae410137079ad850a024
Hassen Taidirt <htaidirt@gmail.com>
2019-09-19 18:03:12 +0200

    Add test engine

---
+++ server.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('./server');
+
+describe('GET /', function () {
+    it('respond with status 200', function (done) {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+});
+
